dc4e97deb50b709087595703f0bd14054eadaaa5
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2021-03-19 09:00:08 +0200

    now uses NB to take a part of data for training. Also displays accuracy

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 # from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-from io import StringIO
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
 import warnings
 
 # remove warnings
@@ -17,11 +19,6 @@ def warn(*args, **kwargs):
 
 
 warnings.warn = warn
-
-# drop last column with classes and divide into 3 clusters
-# - add a feature that it lets you choose how much data we want for training
-# = it needs to show accuracy
-# - output results into a file
 
 # import data
 df = pd.read_csv('iris.data.csv')
@@ -50,8 +47,20 @@ data = df[iris]
 
 # perform clustering here
 clustering_kmeans = KMeans(n_clusters=3, precompute_distances="auto", n_jobs=-1)
-data['clusters'] = clustering_kmeans.fit_predict(data)
+data['CLUSTERS'] = clustering_kmeans.fit_predict(data)
 print("PREDICTED CLUSTERS \n", data.to_string())
+
+inputs = data.drop('CLUSTERS', axis='columns')
+target = data.CLUSTERS
+
+dummies_sepal_length_cm = pd.get_dummies(inputs.sepal_length_cm)
+dummies_sepal_length_cm.head(3)
+dummies_sepal_width_cm = pd.get_dummies(inputs.sepal_width_cm)
+dummies_sepal_width_cm.head(3)
+
+inputs = pd.concat([dummies_sepal_width_cm, dummies_sepal_length_cm, inputs], axis='columns')
+# inputs.head
+print(inputs)
 
 # run PCA (Principal Component Analysis) on the data and reduce dimensions in pca_num_components dimensions
 pca_num_components = 4
@@ -59,9 +68,58 @@ reduced_data = PCA(n_components=pca_num_components).fit_transform(data)
 results = pd.DataFrame(reduced_data, columns=['pca1', 'pca2', 'pca3', 'pca4'])
 
 # visualize data
-sns.scatterplot(x="pca1", y="pca2", hue=data['clusters'], data=results)
+sns.scatterplot(x="pca1", y="pca2", hue=data['CLUSTERS'], data=results)
 # plt.title('K-means Clustering with 2 dimensions')
 plt.show()
+
+# now we use sklearn strain test split method to split our data into training and test sample
+X_train, X_test, y_train, y_test = train_test_split(inputs, target, test_size=0.20)  # train/test samples (90/10)
+
+print("\n-----------------------")
+print("SPLIT DATA: \n")
+print("X_train: ")
+print(len(X_train))
+print("Y_train: ")
+print(len(y_train))
+print("X_test: ")
+print(len(X_test))
+print("Y_test: ")
+print(len(y_test))
+print("Inputs: ")
+print(len(data))
+print("-----------------------\n")
+
+print("\n-----------------------")
+print("X_train data: \n")
+print(X_train)
+print("-----------------------\n")
+print("\n-----------------------")
+print("y_train data: \n")
+print(y_train)
+print("-----------------------\n")
+
+# Now we can use NB model
+model = GaussianNB()
+
+# we use fit method whenever we want to train the model
+model.fit(X_train, y_train)  # when we execute this it will train it
+
+print("\n-----------------------")
+print('X_test[:10]:')
+print(X_test[:10])
+
+print('\ny_test[:10]:')
+print(y_test[:10])
+
+print('\npredict: ')
+print(model.predict(X_test[:10]))
+print("-----------------------\n")
+
+# after training first thing we can do is measure the score to find the accuracy
+print("\n-----------------------")
+print("ACCURACY:")
+print(model.score(X_test, y_test))
+print("-----------------------\n")
 
 # ----------------------------
 # clustering_kmeans = KMeans(n_clusters=3).fit(df)
